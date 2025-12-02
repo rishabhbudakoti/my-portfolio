2fbac1c68660c77e482c23a69d3774818ae17dbf
rishabhbudakot <rishabh44567@gmail.com>
2025-12-02 16:39:25 +0530

    update

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
       <div className="absolute top-0 left-0 leading-0 bg-[linear-gradient(to_bottom,transparent_95%,white_95%),linear-gradient(to_right,transparent_95%,white_95%)] opacity-2 bg-size-[20px_20px] -z-4 w-full h-full"></div>
 
       <div className="flex flex-col justify-center py-20 md:py-28 lg:py-36 items-center px-4">
-        <div className="flex items-center justify-center border text-ternary rounded-full w-20 h-20 md:w-24 md:h-24 mb-6 bg-[rgba(255,255,255,0.05)]">
+        <div className="flex items-center justify-center border backdrop-blur-md text-ternary rounded-full w-20 h-20 md:w-24 md:h-24 mb-6 bg-[rgba(255,255,255,0.05)]">
           <img src={mern} alt="" className="w-14 md:w-18" />
         </div>
 
@@ -51,7 +51,7 @@ const Hero = () => {
               <a key={i} href={`#${d.link.toLowerCase()}`}>
                 <button
                   key={i}
-                  className="border cursor-pointer bg-[rgba(255,255,255,0.05)] border-ternary text-base sm:text-lg text-primary font-semibold w-full sm:w-40 md:w-52 py-3 rounded-4xl capitalize hover:-translate-y-2 transition-all duration-200 hover:shadow-[0px_10px_20px_0px_#5E936C] hover:border-[#5E936C]"
+                  className="border cursor-pointer bg-[rgba(255,255,255,0.05)] backdrop-blur-md border-ternary text-base sm:text-lg text-primary font-semibold w-full sm:w-40 md:w-52 py-3 rounded-4xl capitalize hover:-translate-y-2 transition-all duration-200 hover:shadow-[0px_10px_20px_0px_#5E936C] hover:border-[#5E936C]"
                 >
                   {d.btnName}
                 </button>
